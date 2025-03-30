e0820c581711bcd7d7385f6e3864a9e6737a3ed2
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2025-03-30 14:39:25 +0700

    create reusable components basic for button, modal, and table

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,9 @@
+<flux:modal.trigger name="{{ $triggerModal }}">
+    <flux:button>{{ $buttonModal }}</flux:button>
+</flux:modal.trigger>
+
+<flux:modal name="{{ $triggerModal }}" class="md:w-96">
+    <div class="space-y-6">
+        {{ $modalInputForm }}
+    </div>
+</flux:modal>
---
+++ resources/views/components/table.blade.php
@@ -0,0 +1,17 @@
+
+<div class="relative overflow-hidden shadow-md rounded-lg">
+    <table class="table-fixed w-full text-left">
+        <thead class="uppercase bg-black text-[#e5e7eb]" style="background-color: #black; color: #e5e7eb;">
+           {{ $tableHead  }}
+        </thead>
+        <tbody class="bg-white text-gray-500 bg-[#FFFFFF] text-[#6b7280]" style="background-color: #FFFFFF; color: #6b7280;">
+            <!--[-->
+
+
+            {{ $tableBody }}
+
+
+            <!--]-->
+        </tbody>
+    </table>
+</div>
