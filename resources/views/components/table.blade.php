92bdcbcd11db1f735b4b8fd1e27d27dfa4a4d00c
Radithya Ihza <ihzabowo96@gmail.com>
2025-04-02 12:19:41 +0700

    updated table style

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,17 +1,18 @@
-
-<div class="relative overflow-hidden shadow-md rounded-lg">
-    <table class="table-fixed w-full text-left">
-        <thead class="uppercase bg-black text-[#e5e7eb]" style="background-color: #black; color: #e5e7eb;">
-           {{ $tableHead  }}
-        </thead>
-        <tbody class="bg-white text-gray-500 bg-[#FFFFFF] text-[#6b7280]" style="background-color: #FFFFFF; color: #6b7280;">
-            <!--[-->
-
-
-            {{ $tableBody }}
+<div class="overflow-x-auto rounded-lg shadow text-sm">
+    <div class="min-w-full inline-block">
+        <table class="min-w-full table-auto whitespace-nowrap">
+            <thead class="uppercase">
+                {{ $tableHead  }}
+            </thead>
+            <tbody">
+                <!--[-->
 
 
-            <!--]-->
-        </tbody>
-    </table>
+                {{ $tableBody }}
+
+
+                <!--]-->
+                </tbody>
+        </table>
+    </div>
 </div>
